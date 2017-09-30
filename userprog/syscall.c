ad15dee6cfc9199144137f8ffb84888b3d12b4de
Neel120 <neelmittal007@gmail.com>
2017-09-30 18:09:23 +0530

    UP03 Finished

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,12 +1,21 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <list.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "threads/init.h"
+#include "filesys/filesys.h"
+#include "filesys/file.h"
 
 static void syscall_handler (struct intr_frame *);
+
+struct file_struct{
+	int fd;
+	struct file * file;
+	struct list_elem file_elem;
+};
 /* Reads a byte at user virtual address UADDR.
    UADDR must be below PHYS_BASE.
    Returns the byte value if successful, -1 if a segfault
@@ -32,6 +41,18 @@ put_user (uint8_t *udst, uint8_t byte)
   return error_code != -1;
 }
 
+/* Obtains the file from the file descriptor assigned. */
+struct file * getfile(int fd)
+{
+	struct list_elem * e;
+	struct thread * t = thread_current ();
+	for(e = list_begin(&(t->files));e!=list_end(&(t->files)); e = list_next(e))
+	{
+		struct file_struct * fp = list_entry(e,struct file_struct, file_elem);
+		if(fp->fd==fd) return fp->file;
+	}
+	return NULL;
+}
 
 void
 syscall_init (void) 
@@ -56,23 +77,45 @@ int sys_wait (int child_pid){
 }
 
 bool sys_create (const char *file, unsigned initial_size){
-	printf("system call\n");
+	if(is_user_vaddr(file)==0 || get_user(file) == -1 || file==NULL) sys_exit(-1);
+	return filesys_create(file,initial_size);
 }
 
 bool sys_remove (const char *file){
-	printf("system call!\n");
+	if(is_user_vaddr(file)==0 || get_user(file) == -1 || file==NULL) sys_exit(-1);
+	return (filesys_remove(file)!=NULL);
 }
 
 int sys_open (const char *file){
-	printf("system call!\n");
+	if(is_user_vaddr(file)==0 || get_user(file) == -1 || file==NULL) sys_exit(-1);
+	struct file_struct * fp = malloc(sizeof(struct file_struct));
+	fp->file = filesys_open(file);
+	if(fp->file == NULL)
+		return -1;
+
+	list_push_back(&(thread_current ()->files),&(fp->file_elem));
+	fp->fd = (thread_current() ->fd_last)++;
+	return fp->fd;
 }
 
 int sys_filesize (int fd){
-	printf("system call!\n");
+	return file_length(getfile(fd));
 }
 
 int sys_read (int fd, void *buffer, unsigned length){
-	printf("system call!\n");
+	if(is_user_vaddr(buffer)==0 || get_user(buffer) == -1 || get_user(buffer+length) == -1)
+		thread_exit(-1);
+
+	if(fd==0){
+
+	}
+
+	struct file * file = getfile(fd);
+	if(file==NULL){
+		return 0;
+	}
+
+	return file_read(file,buffer,length);
 }
 
 int sys_write (int fd, const void *buffer, unsigned length){
@@ -85,18 +128,34 @@ int sys_write (int fd, const void *buffer, unsigned length){
 			return length;
 	}
 
+	struct file * file = getfile(fd);
+	if(file==NULL){
+		return 0;
+	}
+
+	return file_write(file,buffer,length);
 }
 
 void sys_seek (int fd, unsigned position){
-	printf("system call\n");
+	file_seek(getfile(fd),position);
 }
 
 unsigned sys_tell (int fd){
-	printf("system call\n");
+	return file_tell(getfile(fd));
 }
 
 void sys_close (int fd){
-	printf("system call\n");
+	struct list_elem * e;
+	struct thread * t = thread_current ();
+	for(e = list_begin(&(t->files));e!=list_end(&(t->files)); e = list_next(e))
+	{
+		struct file_struct * fp = list_entry(e,struct file_struct, file_elem);
+		if(fp->fd==fd){
+			list_remove(e);
+			file_close(fp->file);
+			return;
+		}
+	}
 }
 
 int verify(struct intr_frame *f){
@@ -160,24 +219,32 @@ syscall_handler (struct intr_frame *f)
   	case 3: f->eax = sys_wait(*(p+1));
   			break;
 
-  	case 4:
+  	case 4: f->eax = sys_create(*(p+1),*(p+2));
+  			break;
 
-  	case 5:
+  	case 5: f->eax = sys_remove(*(p+1));
+  			break;
 
-  	case 6:
+  	case 6:	f->eax = sys_open(*(p+1));
+  			break;
 
-  	case 7:
+  	case 7: f->eax = sys_filesize(*(p+1));
+  			break;
 
-  	case 8:
+  	case 8: f->eax = sys_read(*(p+1),*(p+2),*(p+3));
+  			break;
 
   	case 9: f->eax = sys_write(*(p+1),*(p+2),*(p+3));
   			break;
-  	case 10:
+  	
+  	case 10: sys_seek(*(p+1),*(p+2));
+  			 break;
 
-  	case 11:
+  	case 11: f->eax = sys_tell(*(p+1));
+  			 break;
 
-  	case 12:
-  		break;
+  	case 12: sys_close(*(p+1));
+  			 break;
   }
 
 }
